984f3d27387043457d90920466bd47acbdddfe81
thuannguyen88 <getaholdofthuan@gmail.com>
2022-03-16 17:37:05 +0000

    create input form component with input fields for location and destination

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import InputForm from "./InputForm/InputForm.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>google maps api</h1>
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
